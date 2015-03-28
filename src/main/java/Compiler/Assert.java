94793f6a97498aed47771f3eaaaf12c2596dfff5
MathiasB <git@denbeke.be>
2015-03-28 17:22:16 +0100

    Enhanced Assert class

--- src/main/java/Compiler/Assert.java
+++ src/main/java/Compiler/Assert.java
@@ -5,16 +5,29 @@ package Compiler;
  */
 public class Assert {
 
-	public static void Assert(boolean a) {
 
+    /**
+     * Assert
+     *
+     * @param a: condition
+     */
+	public static void Assert(boolean a) {
 		if(!a) {
-			// System.out.println("[ERROR] Assertion failed: " +
-			// Thread.currentThread().getStackTrace()[2] );
-			System.out.println((char) 27 + "[31m"
-					+ "[ERROR] Assertion failed: "
-					+ Thread.currentThread().getStackTrace()[2] + (char) 27
-					+ "[0m");
-			System.exit(1);
+            Log.fatal("[ERROR] Assertion failed: " + Thread.currentThread().getStackTrace()[2], Thread.currentThread().getStackTrace()[2].getLineNumber());
 		}
 	}
+
+
+    /**
+     * Assert
+     *
+     * @param a:   condition
+     * @param msg: message
+     */
+    public static void Assert(boolean a, String msg) {
+        if(!a) {
+            Log.fatal("[ERROR] Assertion failed '" + msg + "': " + Thread.currentThread().getStackTrace()[2], Thread.currentThread().getStackTrace()[2].getLineNumber());
+        }
+    }
+
 }
