6bb13256de9f44bde67bfb0bd30a7f87f7c4d5f5
MathiasB <git@denbeke.be>
2015-04-24 20:06:13 +0200

    [TEST] return statements

---
+++ src/test/input/semantics/return1_ok.c
@@ -0,0 +1,12 @@
+// Enough returns
+
+int a() {
+    if(0) {
+        return 0;
+    }
+    else {
+        //return 1;
+    }
+    return 0;
+    //return;
+}
---
+++ src/test/input/semantics/return2_ok.c
@@ -0,0 +1,10 @@
+// Enough returns
+
+int a() {
+    if(0) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
