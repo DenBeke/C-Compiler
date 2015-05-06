c76af53d82f1e869be4325660a4d48f1a877992a
MathiasB <git@denbeke.be>
2015-05-06 14:49:18 +0200

    Added more unit tests

---
+++ src/test/input/codegen/if.c
@@ -0,0 +1,24 @@
+// Some conditionals
+
+#include <stdio.h>
+
+void main() {
+
+    if(0) {
+        print("Fail\n");
+    } else {
+        print("Ok\n");
+    }
+
+    if(0) {
+        print("Fail\n");
+    }
+
+    if(1) {
+        print("Ok\n");
+    }
+    else {
+        print("Fail\n");
+    }
+
+}
---
+++ src/test/input/codegen/while.c
@@ -0,0 +1,13 @@
+// While loops
+
+#include <stdio.h>
+
+void main() {
+    
+    int a = 100;
+    while(a >= 99) {
+        printf("%d\n", a);
+        a = a - 1;
+    }
+    
+}
